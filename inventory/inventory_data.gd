c4502c4dfbdc114162893a050869ec148de975e1
CungSao <2022bevy@gmail.com>
2024-04-16 18:10:02 +0700

    Merging Pick Ups 54:53

--- inventory/inventory_data.gd
+++ inventory/inventory_data.gd
@@ -30,6 +30,22 @@ func drop_slot_data(grabbed_slot_data, index) -> SlotData:
 	#return slot_data
 
 
+func pick_up_slot_data(slot_data:SlotData) -> bool:
+	for index in slot_datas.size():
+		if slot_datas[index] and slot_datas[index].can_fully_merge_with(slot_data):
+			slot_datas[index].fully_merge_with(slot_data)
+			inventory_updated.emit(self)
+			return true
+			
+	for index in slot_datas.size():
+		if !slot_datas[index]:
+			slot_datas[index] = slot_data
+			inventory_updated.emit(self)
+			return true
+			
+	return false
+
+
 func drop_single_slot_data(grabbed_slot_data, index) -> SlotData:
 	var slot_data = slot_datas[index]
 	
