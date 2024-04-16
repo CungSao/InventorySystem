5676e710d5eeda69d17d5a3df1ad58d19f427282
CungSao <2022bevy@gmail.com>
2024-04-16 17:30:18 +0700

    Clear External Inventory 48:54

--- inventory/InventoryInterface.gd
+++ inventory/InventoryInterface.gd
@@ -1,18 +1,40 @@
 extends Control
 
 var grabbed_slot_data:SlotData
+var external_inventory_owner
 
 @onready var player_inventory = $PlayerInventory
 @onready var grabbed_slot = $GrabbedSlot
+@onready var external_inventory = $ExternalInventory
 
 func _physics_process(delta):
 	if grabbed_slot.visible:
 		grabbed_slot.global_position = get_global_mouse_position() + Vector2(5,5)
 
+
 func set_player_invetory_data(inventory_data:InventoryData):
 	inventory_data.inventory_interact.connect(on_inventory_interact)
 	player_inventory.set_inventory_data(inventory_data)
 
+
+func set_external_inventory(_external_inventory_owner):
+	external_inventory_owner = _external_inventory_owner
+	var inventory_data = external_inventory_owner.inventory_data
+
+	inventory_data.inventory_interact.connect(on_inventory_interact)
+	external_inventory.set_inventory_data(inventory_data)
+	external_inventory.show()
+
+
+func clear_external_inventory():
+	if external_inventory_owner:
+		var inventory_data = external_inventory_owner.inventory_data
+
+		inventory_data.inventory_interact.disconnect(on_inventory_interact)
+		external_inventory.clear_inventory_data(inventory_data)
+		external_inventory.hide()
+		external_inventory_owner = null
+	
 func on_inventory_interact(inventory_data:InventoryData,
 		index:int, button:int):
 			
@@ -29,6 +51,7 @@ func on_inventory_interact(inventory_data:InventoryData,
 
 	#print(grabbed_slot_data)
 	update_grabbed_slot()
+
 
 func update_grabbed_slot():
 	if grabbed_slot_data:
