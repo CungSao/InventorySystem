e7fd0ae698cfe6cf4be38515672e0b516ffd512d
CungSao <2022bevy@gmail.com>
2024-04-16 14:17:13 +0700

    Invetory Interact Signal

--- inventory/inventory_data.gd
+++ inventory/inventory_data.gd
@@ -1,6 +1,7 @@
 class_name InventoryData extends Resource
 
+signal inventory_interact(inventory_data:InventoryData, index:int, button:int)
 @export var slot_datas:Array[SlotData]
 
 func on_slot_clicked(index:int, button:int):
-	print("inv")
+	inventory_interact.emit(self, index, button)
